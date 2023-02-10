aafdd4000faa039e82634c203768fce5c5cc2acb
Petar Ganchev <PetarGanchev@users.noreply.github.com>
2023-02-10 14:58:19 +0200

    v.0.25.0

--- ЕЕДОП by Petar Ganchev.user.js
+++ ЕЕДОП by Petar Ganchev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ЕЕДОП by Petar Ganchev
 // @namespace    http://tampermonkey.net/
-// @version      0.3.0
+// @version      0.25
 // @description  try to take over the world!
 // @author       Petar Ganchev
 // @match        */espd-web/*
